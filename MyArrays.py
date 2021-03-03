04ba221524993ec0bd2d4c21e3074ff84258bfe8
Abby Brown <abby_brown3@baylor.edu>
2021-03-03 15:42:58 -0600

    OUCH

---
+++ MyArrays.py
@@ -0,0 +1,16 @@
+# command palette + REPL = interactive mode
+##disadvantage: nothing is saved! (think the ide from python 1)
+import numpy as np
+
+# List Comprehension
+# Create a one-dimensional array from a list
+# that produces even integers from 2 through 20
+
+integers = np.array([x for x in range(2, 21, 2)])
+print(integers)
+
+INTEGERS = NP.ARRAY([[1, 2, 3], [4, 5, 6]])
+PRINT(INTEGERS)
+
+floats = np.array([0.0, 0.1, 0.2, 0.3])
+print(floats)
